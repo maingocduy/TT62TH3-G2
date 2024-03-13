3ca5581c8ea87cf00e02812f2175f5d579607ab8
dungbeo26 <2051063957@e.tlu.edu.vn>
2024-03-14 01:50:06 +0700

    Create Demo1View.vue

---
+++ vue-project/src/views/Demo1View.vue
@@ -0,0 +1,15 @@
+<template>
+    <Header></Header>
+    <Demo1Page/>
+    <Footer></Footer>
+</template>
+
+<script setup >
+import Footer from '@/components/layout/Footer.vue';
+import Header from '@/components/layout/Header.vue';
+import Demo1Page from '@/components/pages/Demo1Page.vue';
+</script>
+
+<style scoped>
+
+</style>

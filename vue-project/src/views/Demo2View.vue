e633af9cd40ea1bcf53e72dd8cfc91a489ec904c
dungbeo26 <2051063957@e.tlu.edu.vn>
2024-03-14 01:50:11 +0700

    Create Demo2View.vue

---
+++ vue-project/src/views/Demo2View.vue
@@ -0,0 +1,15 @@
+<template>
+    <Header></Header>
+    <Demo2Page/>
+    <Footer></Footer>
+</template>
+
+<script setup >
+import Footer from '@/components/layout/Footer.vue';
+import Header from '@/components/layout/Header.vue';
+import Demo2Page from '@/components/pages/Demo2Page.vue';
+</script>
+
+<style scoped>
+
+</style>

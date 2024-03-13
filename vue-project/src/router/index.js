7b35cd95610b7bb691ac8627cf5ead52b255bdfe
dungbeo26 <2051063957@e.tlu.edu.vn>
2024-03-14 01:50:04 +0700

    Update index.js

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MissionView from '../views/MissionView.vue'
+
+import Demo1View from '../views/Demo1View.vue'
+import Demo2View from '../views/Demo2View.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,7 +20,17 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: MissionView
-    }
+    },
+    {
+      path: '/demo1',
+      name: 'Demo1',
+      component: Demo1View
+    },
+    {
+      path: '/demo2',
+      name: 'Demo2',
+      component: Demo2View
+    },
   ]
 })
 

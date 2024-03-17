8b0ac07c58f103e6c5675bd46634fecc30c45677
dungbeo26 <2051063957@e.tlu.edu.vn>
2024-03-17 15:41:10 +0700

    Update main.js

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -9,3 +9,15 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+import axios from 'axios';
+
+axios.get('https://api.example.com/data')
+  .then(response => {
+    // Xử lý phản hồi
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Xử lý lỗi
+    console.error('Lỗi khi truy vấn dữ liệu:', error);
+  });
